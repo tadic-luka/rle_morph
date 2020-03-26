339484240ed94c521399d0cae25e91cbafe06159
Luka Tadić <luka.tadic@fer.hr>
2020-03-26 12:08:37 +0100

    improve time measurement of dilation in example and rename example

--- examples/dilate.rs
+++ examples/dilate.rs
@@ -1,7 +1,7 @@
 use rle_morph::{Image, Run, RLE};
 use std::path::Path;
 use imageproc::distance_transform::Norm;
-use imageproc::morphology::{dilate, dilate_mut};
+use imageproc::morphology::dilate_mut;
 use image::GrayImage;
 
 fn load_image<P: AsRef<Path>>(path: P) -> GrayImage {
@@ -17,14 +17,7 @@ fn clone_to_image(img: &GrayImage) -> Image {
 fn clone_to_gray_image(img: &Image) -> GrayImage {
     let clone = img.clone();
     let (width, height) = (img.w(), img.h());
-    GrayImage::from_raw(width as _, height as _, clone.into_raw()).unwrap()
-}
-
-fn dilate_rle(img: Image) -> Image {
-    let mut rle = RLE::from(&img);
-    let dilated = rle.dilate(&RLE::l1_structuring(1)).to_image();
-    let (w, h) = (dilated.w(), dilated.h());
-    let mut img_data = dilated.into_raw();
+    let mut img_data = clone.into_raw();
     // binary image is 0s and 1s, convert all 1s to white
     for val in &mut img_data {
         *val = if *val > 0 {
@@ -33,27 +26,23 @@ fn dilate_rle(img: Image) -> Image {
             0
         };
     }
-    Image::new(w, h, img_data)
-}
-
-fn dilate_imageproc(mut img: GrayImage) -> GrayImage {
-    dilate_mut(&mut img, Norm::L1, 1);
-    img
+    GrayImage::from_raw(width as _, height as _, img_data).unwrap()
 }
 
 fn main() {
-    let gray_image = load_image(std::env::args().skip(1).next().expect("provide path to png image"));
+    let mut gray_image = load_image(std::env::args().skip(1).next().expect("provide path to png image"));
 
     // dilate using rle
     let img: Image = clone_to_image(&gray_image);
+    let rle = RLE::from(&img);
     let start = std::time::Instant::now();
-    let img = dilate_rle(img);
-    println!("Time took to dilate with rle: {} ms", start.elapsed().as_millis());
+    let img = rle.dilate(&RLE::linf_structuring(5)).to_image();
+    println!("Time took to dilate with rle and decode: {} us", start.elapsed().as_micros());
     clone_to_gray_image(&img).save("rle_dilate.png").unwrap();
 
     // dilate using imageproc
     let start = std::time::Instant::now();
-    let gray = dilate_imageproc(gray_image);
-    println!("Time took to dilate with imageproc: {} ms", start.elapsed().as_millis());
-    gray.save("imageproc_dilate.png").unwrap();
+    dilate_mut(&mut gray_image, Norm::LInf, 5);
+    println!("Time took to dilate with imageproc: {} us", start.elapsed().as_micros());
+    gray_image.save("imageproc_dilate.png").unwrap();
 }
